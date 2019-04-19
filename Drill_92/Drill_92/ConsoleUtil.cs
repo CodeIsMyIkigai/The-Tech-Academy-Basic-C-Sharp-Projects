e65513f820e9d9a9e2b8b74084fee9e905980f1e
CodeIsMyIkigai <Chester.M.Terry@gmail.com>
2019-04-19 15:32:24 -0700

    Initial Load of Drill_92 and Utility Class

---
+++ Drill_92/Drill_92/ConsoleUtil.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drill_92
+{
+    class ConsoleUtil
+    {
+        /* Get a string from the console */
+
+        //Get a valid string result from the console with two questions.
+        public static string GetConsoleString(string question, string requestion)
+        {
+            Console.WriteLine(question);
+            string result = Console.ReadLine();
+            while (string.IsNullOrEmpty(result))
+            {
+                Console.WriteLine(requestion);
+                result = Console.ReadLine();
+            }
+            return result;
+        }
+        //Get a valid string result from the console with question only.
+        private static string GetConsoleString(string question)
+        {
+            string result = GetConsoleString(question, "Please enter a valid non-empty string.");
+            return result;
+        }
+        //Get a valid string result from the console with no questions.
+        public static string GetConsoleString()
+        {
+            string result = GetConsoleString("Please enter a valid non-empty string.", "Please enter a valid non-empty string.");
+            return result;
+        }
+
+
+        /* Get an int from the console */
+
+        //Get a valid string result from the console with question and response.
+        public static int GetConsoleInt(string question, string requestion)
+        {
+            int result;
+            Console.WriteLine(question);
+            string intIn = Console.ReadLine();
+            while (!int.TryParse(intIn, out result))
+            {
+                Console.WriteLine(requestion);
+                intIn = Console.ReadLine();
+            }
+            return result;
+        }
+
+        //Get a valid string result from the console with only question.
+        public static int GetConsoleInt(string question)
+        {
+            int result = GetConsoleInt(question, "Please enter a valid integer.");            
+            return result;
+        }
+
+        //Get a valid string result from the console with no questions.
+        public static int GetConsoleInt()
+        {
+            int result = GetConsoleInt("Please enter a valid integer.", "Please enter a valid integer.");
+            return result;
+        }
+
+        /* Get an float from the console */
+
+        //Get a valid float result from the console with question and response.
+        public static float GetConsoleFloat(string question, string requestion)
+        {            
+            float result;
+            Console.WriteLine(question);
+            string floatIn = Console.ReadLine();
+            while (!float.TryParse(floatIn, out result))
+            {
+                Console.WriteLine(requestion);
+                floatIn = Console.ReadLine();
+            }
+            return result;
+        }
+
+        //Get a valid float result from the console with only question.
+        public static float GetConsoleFloat(string question)
+        {
+            float result = GetConsoleFloat(question, "Please enter a valid decimal number.");
+            return result;
+        }
+
+        //Get a valid string result from the console with no questions.
+        public static float GetConsoleFloat()
+        {
+            float result = GetConsoleFloat("Please enter a valid decimal number.", "Please enter a valid decimal number.");
+            return result;
+        }
+
+        /* Get a boolean from the console */
+
+        //Get a valid bool result from the console.
+        public static bool GetConsoleBool(string question, string requestion)
+        {
+            bool result;
+            Console.WriteLine(question);
+            string helpNeeded = Console.ReadLine();
+            while (!Boolean.TryParse(helpNeeded, out result))
+            {
+                Console.WriteLine(requestion);
+                helpNeeded = Console.ReadLine();
+            }
+            return result;
+        }
+        
+        //Get a valid bool result from the console.
+        public static bool GetConsoleBool(string question)
+        {
+            bool result = GetConsoleBool(question, "Please enter either 'true' or 'false'.");
+            return result;
+        }
+
+        //Get a valid bool result from the console.
+        public static bool GetConsoleBool()
+        {
+            bool result = GetConsoleBool("Please enter either 'true' or 'false'.", "Please enter either 'true' or 'false'.");
+            return result;
+        }
+    }
+}
---
+++ Drill_92/Drill_92/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+/*
+
+C# Pg 92
+ 
+EXERCISE: BRANCHING
+Your job is to create a console-based application for getting a shipping quote for a package.
+
+REQUIREMENTS
+- Dimensions
+    - Weight
+        - The first line of the program must be: “Welcome to Package Express. Please follow the instructions below.”
+        - The user must then be prompted for the package weight.
+
+    - If the weight is greater than 50, display the error message, 
+    “Package too heavy to be shipped via Package Express. Have a good day.” At this point the program would end.
+
+    - Width
+        - The user must then be prompted for the package width.
+    - Height
+        - Then the package height.
+    - Length
+        - Then the package length.
+
+- Dimension Check
+    - If the sum of the dimensions is greater than 50, display the error message, “Package too big to be shipped via Package Express.” At this point the program would end.
+
+- Quote
+    - The sum total of dimensions are then multiplied by the weight and divided by 100.
+        - The result of that calculation is the quote.
+        - Display the quote to the user as a dollar amount.
+
+*/
+
+
+
+namespace Drill_92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            float weight = ConsoleUtil.GetConsoleFloat(
+                "Please enter a valid decimal number for package weght.",
+                "Please enter a valid decimal number."
+                );
+            if(weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+                Console.ReadLine();
+                return;
+            }
+                        
+            float width = ConsoleUtil.GetConsoleFloat(
+                "Please enter a valid decimal number for package width.",
+                "Please enter a valid decimal number."
+                );
+            float height = ConsoleUtil.GetConsoleFloat(
+                "Please enter a valid decimal number for package height.",
+                "Please enter a valid decimal number."
+                );
+            float length = ConsoleUtil.GetConsoleFloat(
+                "Please enter a valid decimal number for length height.",
+                "Please enter a valid decimal number."
+                );
+            float sum = (width + height + length);
+            if ( sum > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return;
+            }
+
+            float estimate = (sum * weight / 100);
+
+            Console.WriteLine("Your estimated total for shipping this package is: {0:C2}", estimate);
+            Console.ReadLine();
+        }
+    }
+}
