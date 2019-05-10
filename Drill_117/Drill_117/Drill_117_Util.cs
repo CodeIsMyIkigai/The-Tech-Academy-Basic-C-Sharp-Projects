639e123ff080fbe94739fae2065bdf321389e473
cterry <chester.m.terry@gmail.com>
2019-05-10 16:04:19 -0700

    Initial Load of Drill_117

---
+++ Drill_117/Drill_117/Drill_117_Util.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+1. Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
+
+2. In the Main() method, instantiate that class.
+
+3. Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two.
+
+4. Create a method with output parameters.
+
+5. Overload a method.
+
+6. Declare a class to be static.
+ */
+
+
+namespace Drill_117
+{
+    static class Drill_117_Util
+    {
+        //Requirement of void method with and output parameter.
+        public static void DivideByTwo(decimal input, out int output)
+        {
+            output = (int)(input / 2);
+        }
+
+        //Requirements of overloaded method.
+        public static void DivideByTwo(string input, out int output)
+        {
+            decimal dOutput;
+            decimal.TryParse(input, out dOutput);
+            output = (int)(dOutput / 2);
+        }
+    }
+}
---
+++ Drill_117/Drill_117/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+/*
+
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+    1. Create a class. In that class, create a void method that outputs an integer. 
+        Have the method divide the data passed to it by 2.
+
+    2. In the Main() method, instantiate that class.
+
+    3. Have the user enter a number. Call the method on that number. Display the output to the screen. 
+        It should be the entered number, divided by two.
+
+    4. Create a method with output parameters.
+
+    5. Overload a method.
+
+    6. Declare a class to be static.   
+     
+*/
+
+namespace Drill_117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            bool good = false;
+            decimal dInput = 0.00m;
+            string input;
+            while(!good)
+            {
+                Console.WriteLine("Enter a number.");
+                input = Console.ReadLine();
+                if (decimal.TryParse(input, out dInput))
+                {
+                    good = true;
+                }
+            }
+
+            Drill_117_Util.DivideByTwo(dInput, out int output);
+            Console.WriteLine("Result of dividing {0} by 2 is {1}", dInput, output);
+
+            string twelve = "12";
+
+            Drill_117_Util.DivideByTwo(twelve, out output);
+            Console.WriteLine("Result of dividing {0} by 2 is {1}", twelve, output);
+
+            Console.ReadLine();
+        }
+    }
+}
