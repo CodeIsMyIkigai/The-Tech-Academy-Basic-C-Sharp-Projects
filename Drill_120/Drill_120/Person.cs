f16404c4cd2aa0fb9303d7f14021dd360feef454
cterry <chester.m.terry@gmail.com>
2019-05-15 14:59:03 -0700

    Initial Load of Drill_120

---
+++ Drill_120/Drill_120/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drill_120
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+
+
+    }
+}
