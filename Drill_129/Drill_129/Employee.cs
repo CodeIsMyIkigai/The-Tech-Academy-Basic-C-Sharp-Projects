d4cc039df6c5df5e2eaf4698af1580cbd4831323
CodeIsMyIkigai <Chester.M.Terry@gmail.com>
2019-05-16 18:17:59 -0700

    Initial Load of Drill 129

---
+++ Drill_129/Drill_129/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drill_129
+{
+    class Employee : Person
+    {
+        public int Id { get; set; }
+
+        //Quick Action autogeneration
+        public override bool Equals(object obj)
+        {
+            var employee = obj as Employee;
+            return employee != null &&
+                   Id == employee.Id;
+        }
+
+        //Quick Action autogeneratio
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            if(emp1.Id == emp2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            if(emp1.Id != emp2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Drill_129/Drill_129/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/*
+EXERCISE:
+
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+
+Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+     
+     
+*/
+
+
+namespace Drill_129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee one = new Employee() { FirstName = "Instructor", LastName = "Ben", Id = 1 };
+            Employee two = new Employee() { FirstName = "Instructor", LastName = "Jan", Id = 2 };
+            Employee three = new Employee() { FirstName = "Instructor", LastName = "Jan", Id = 2 };
+
+            if (one == two)
+            {
+                Console.WriteLine("If this prints then operator overloading failed!!");
+            }
+            else
+            {
+                Console.WriteLine("Employees are not equal by Id!");
+
+            }   
+            
+            if( two == three)
+            {
+                Console.WriteLine("Employees are equal by Id!");
+            }
+
+            Console.ReadLine(); //need so see the output
+        }
+    }
+}
