6ab8e53f53e315c51382979dcc7d1358c1a083b8
CodeIsMyIkigai <Chester.M.Terry@gmail.com>
2019-05-25 19:42:05 -0700

    Initial Load of Drill_162 and new .gitignore.

---
+++ Drill_162/Drill_162/DrillClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+EXERCISE:
+1. Create a const variable
+
+2. Create a variable using the keyword "var".
+
+3. Chain two constructors together.
+*/
+
+
+
+namespace Drill_162
+{
+    class DrillClass
+    {
+        public bool ThisIsCool { get; set; }
+
+        public DrillClass() : this(true)
+        {
+
+        }
+            
+        public DrillClass(bool cool)
+        {
+            ThisIsCool = cool;
+        }
+    }
+}
---
+++ Drill_162/Drill_162/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+EXERCISE:
+1. Create a const variable
+
+2. Create a variable using the keyword "var".
+
+3. Chain two constructors together. 
+*/
+
+namespace Drill_162
+{
+    class Program
+    {
+
+        const string Key = "This Key String";
+
+        static void Main(string[] args)
+        {
+            var things = new Dictionary<string, DrillClass>();
+
+            DrillClass drill = new DrillClass();
+            things[Key] = drill;
+
+            DrillClass thingy = things[Key];
+
+            Console.WriteLine("Things are fine: {0}", thingy.ThisIsCool);
+            Console.ReadLine();
+        }
+    }
+}
