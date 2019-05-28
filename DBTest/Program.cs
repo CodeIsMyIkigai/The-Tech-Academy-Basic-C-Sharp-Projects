c80a86e7d093c9bb22b23e5132c571188334d0fe
CodeIsMyIkigai <Chester.M.Terry@gmail.com>
2019-05-28 00:15:08 -0700

    Initial Load of DBTest

---
+++ DBTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("DB Test");
+
+                string connectString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MyTemp;
+                 Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;
+                 ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                string queryString = @"insert into Descriptions (Name, Description) 
+                                    values (@Name, @Description)";
+
+                string description = "'hahahah'); drop database;"; //Testing Instructors comments about dividing into parameters
+                                                            // Solves SQL Injection.  I don't think it does.
+
+
+                using (SqlConnection conn = new SqlConnection(connectString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, conn);
+                    command.Parameters.Add("@Name", SqlDbType.VarChar);
+                    command.Parameters.Add("@Description", SqlDbType.VarChar);
+
+                    command.Parameters["@Name"].Value = "Howdy";
+                    command.Parameters["@Description"].Value = description;
+
+                    conn.Open();
+                    int rows = command.ExecuteNonQuery();
+
+                    Console.WriteLine("Rows: {0}", rows);
+                    
+
+
+                }
+
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
